0e01d8c3a1d15c892844ee9152db938f8bd85350
Rick <18540223+R3DEY3UK@users.noreply.github.com>
2018-12-05 16:47:23 +0000

    update for ZER

--- app/components/faq.jsx
+++ app/components/faq.jsx
@@ -14,17 +14,17 @@ export default class ZFaq extends React.Component {
 
             <p>
               <b>Q: How is this secure? Its on a web browser!</b><br/>
-              A: This wallet has replicated the core features of the BitcoinZ daemon in JavaScript! So the process of creating and signing the transactions are all done within the browser. No sensitive information is sent through the network.
+              A: This wallet has replicated the core features of Zero's daemon in JavaScript! So the process of creating and signing the transactions are all done within the browser. No sensitive information is sent through the network.
             </p>
 
             <p>
               <b>Q: Can I have the source code?</b><br/>
-              A: <a href="https://github.com/egyptianbman/mybitcoinzwallet">Here you go</a>
+              A: <a href="https://github.com/zerocurrencycoin">Here you go</a>
             </p>
 
             <p>
               <b>Q: Why are you doing this?</b><br/>
-              A: This wallet was inspired by MyZenWallet which was inspired by <a href="https://myetherwallet.com">MyEtherWallet's</a> mission statement: to provide the people the ability to interact with the ZEN blockchain easily, without having to run a full node.
+              A: This wallet was inspired by MyZenWallet & BitcoinZ, which was inspired by <a href="https://myetherwallet.com">MyEtherWallet's</a> mission statement: to provide the people the ability to interact with the ZER blockchain easily, without having to run a full node.
             </p>
 
           </Col>
